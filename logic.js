271821857bd003ab603f7db61998425734e9c2b5
Nikita <nikitakoulaga@gmail.com>
2019-12-27 19:18:37 -0500

    new design and checkbox functionality

--- logic.js
+++ logic.js
@@ -42,14 +42,57 @@ function printLibrary(){
     }); 
 }
 
+
+//create new div, insert book, add to DOM
 function displayLibraryTableInPage(){
     const display = document.querySelector("#bookContainer");
     
     library.forEach((book) => {
         let div = document.createElement("div");
-        div.className = "book"; 
-        div.textContent = "Title: " + book.title + "\n" +  "Author: " + book.author + "\n" + 
-                        "Length: " + book.pages + "\n" + "Read it?: " + book.readStatus;
+        let textDiv = document.createElement("div");
+        let checkboxDiv = document.createElement("div");
+        
+        //create checkbox element for each added book
+        let checkbox = document.createElement("INPUT");
+        checkbox.setAttribute("type", "checkbox");
+        
+
+        checkboxDiv.id = "checkboxDiv";
+        div.className = "book";
+        textDiv.className = "bookDivText";
+        checkbox.className = "checkbox"; 
+
+        //add event handler to checkbox
+        checkbox.addEventListener("click", () => {
+            changeReadStatus(checkboxDiv, book);
+        })
+
+        //check or uncheck checkbox on inital display
+        if(book.readStatus == true){
+            checkbox.checked = true;
+        }
+        checkboxDivColor(checkbox.checked, checkboxDiv);
+        
+        textDiv.textContent = "Title: " + book.title + "\n" +  "Author: " + book.author + "\n" + 
+                        "Length: " + book.pages;
+        
+        //style the new divs
+        div.style.display = "grid";
+        div.style.gridTemplateColumns = "auto auto"
+        
+        checkboxDiv.style.marginRight = "385px";
+        checkboxDiv.style.cssFloat = "right";
+        checkboxDiv.style.border = "solid";
+        
+        
+        textDiv.style.marginLeft = "415px";
+        textDiv.style.border = "solid";
+        textDiv.style.width = "400px";
+        
+        //append all new elements to DOM
+        checkboxDiv.appendChild(checkbox);
+        div.appendChild(textDiv);                
+        div.appendChild(checkboxDiv);
         display.appendChild(div);
     });
 }
@@ -63,27 +106,33 @@ function buttonClicked(){
     const length = document.querySelector("#lengthInput");
     const status = document.querySelector("#statusInput");
     
-    if(checkInputs){
-        const book = new Book(title.value, author.value, length.value, status.value);
+    if(checkInputs()){
+        const book = new Book(title.value, author.value, length.value, status.checked);
         addBookToLibrary(book);
+    } else{
+        displayLibraryTableInPage();
+        alert("You cannot leave empty fields!");
     }
 
+
+    //reset inputs
     title.value = "";
     author.value = "";
     length.value = "";
-    status.value = "";
+    status.checked = false;
 }
 
 
 //check for no empty inputs
 function checkInputs(){
     let status = true;
-    const inputs = document.querySelectorAll(input);
+    const inputs = document.querySelectorAll(".input");
     inputs.forEach(input => {
         if(input.value == ""){
             status = false;
         }
     })
+    
     return status;
 }
 
@@ -95,4 +144,23 @@ function clearDisplay(){
         child.remove();
     })
 
+}
+
+
+function checkboxDivColor(checked, checkboxDiv){
+    if(checked){
+        checkboxDiv.style.backgroundColor = "green";
+    } else{
+        checkboxDiv.style.backgroundColor = "red";
+    }
+}
+
+function changeReadStatus(checkboxDiv, book){
+    if(!book.readStatus){
+        checkboxDiv.style.backgroundColor = "green";
+        book.readStatus = true;
+    } else{
+        checkboxDiv.style.backgroundColor = "red";
+        book.readStatus = false;
+    }
 }
